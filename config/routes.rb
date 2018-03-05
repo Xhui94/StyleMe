cb88b6aab8d972a1e70bfdd3e482e93d81a5ea75
Xhuiiii <robyn.jh.liu@gmail.com>
2018-03-05 23:59:12 +1100

    Add devise to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { :passwords => "users/passwords", :confirmations => "users/confirmations"}
+  devise_scope :users do
+    get 'signup', to: 'devise/registrations#new'
+    get 'signin', to: 'devise/sessions#new'
+    delete 'signout', to: 'devise/sessions#destroy'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

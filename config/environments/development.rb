d88f2ee78ef3aab426c3aa6e7462a9e36b83599f
Xhuiiii <robyn.jh.liu@gmail.com>
2018-03-06 22:00:24 +1100

    Mailer working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   # devise stuff
   config.action_mailer.default_options = {from: 'no-reply@projectStyle.com'}
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
@@ -46,8 +46,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'localhost:3000',
-    user_name: 'admin',
-    password: 'admin',
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
     authentication: 'plain',
     enable_starttls_auto: true
   }

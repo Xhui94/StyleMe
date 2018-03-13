3d405abc664b13c255940fa0c3a3d980bf56161c
Xhuiiii <robyn.jh.liu@gmail.com>
2018-03-13 11:41:17 +1100

    User and UserContact Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,7 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :styleboards
   has_one :user_contact
   accepts_nested_attributes_for :user_contact
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
--- app/models/user_contact.rb
+++ app/models/user_contact.rb
@@ -2,4 +2,12 @@ class UserContact < ActiveRecord::Base
   belongs_to :user, foreign_key: "user_id"
   belongs_to :type_country, foreign_key: "type_country_id"
   belongs_to :type_region, foreign_key: "type_region_id"
+
+  validates :type_country, presence: true
+  validates :type_region, presence: true
+  validates :address1, presence: true
+  validates :city, presence: true
+  validates :postal_code, presence: true
+  validates :business_number, presence: true, numericality: true, length: {minimum: 10, maximum: 15}
+	validates :cell_number, numericality: true, length: {minimum: 10, maximum: 15}, allow_blank: true
 end
